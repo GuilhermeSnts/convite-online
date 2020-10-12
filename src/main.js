609105e1b705d65269c48a741507a035e93b288d
guilherme santos <guilhermess1994@gmail.com>
2020-10-12 07:57:42 -0300

    removed vuesax

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import "./plugins/vuesax";
-
-import "vuesax/dist/vuesax.css";
 
 Vue.config.productionTip = false;
 
