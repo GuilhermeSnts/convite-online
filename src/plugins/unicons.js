27c9dd47fbe423e28b4e74f7cdd58e4d38562cbf
guilherme santos <guilhermess1994@gmail.com>
2020-10-12 18:07:12 -0300

    add unicons

---
+++ src/plugins/unicons.js
@@ -0,0 +1,10 @@
+import Unicon from "vue-unicons";
+import Vue from "vue";
+
+// import icons to use
+import { uniMapMarker } from "vue-unicons/src/icons";
+
+// add them into unicon
+Unicon.add([uniMapMarker]);
+
+Vue.use(Unicon);
