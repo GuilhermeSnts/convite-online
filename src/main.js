6a067001bce0447b7d1eaac6879412386b1ceeea
guilherme santos <guilhermess1994@gmail.com>
2020-10-24 23:48:25 -0300

    added vuex

--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import "./plugins/unicons.js";
-import "./plugins/maps.js";
-
+import "leaflet/dist/leaflet.css";
+import store from "./store";
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
