3e9fa33877623808c0773a32b7d2163394b83c99
guilherme santos <guilhermess1994@gmail.com>
2020-10-25 01:45:22 -0300

    added header

---
+++ src/components/XHeader.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  .header
+      .header-return.header-item
+        x-button(icon="map-marker" iconColor="#ffffff" label="voltar" mode="outline" to="/")
+      .header-title.header-item
+        h1 {{ title }}
+</template>
+
+<script>
+import XButton from "../components/XButton";
+
+export default {
+  name: "XHeader",
+
+  components: {
+    XButton
+  },
+
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.header
+    height: 80px
+    display: flex
+    flex-direction: row
+    justify-content: space-between
+    align-content: center
+    vertical-align: middle
+    .header-item
+        display: flex
+        height: 100%
+    .header-return
+        margin-left: 10px
+        align-items: center
+        align-content: center
+    .header-title
+        margin-right: 10px
+</style>
