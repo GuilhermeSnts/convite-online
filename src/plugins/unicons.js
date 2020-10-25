1728ddbf0662e647fc3063f7645ee2bd3529cf8b
guilherme santos <guilhermess1994@gmail.com>
2020-10-25 11:25:08 -0300

    add icon Calender

--- src/plugins/unicons.js
+++ src/plugins/unicons.js
@@ -2,9 +2,9 @@ import Unicon from "vue-unicons";
 import Vue from "vue";
 
 // import icons to use
-import { uniMapMarker, uniArrowLeft } from "vue-unicons/src/icons";
+import { uniMapMarker, uniArrowLeft, uniCalender } from "vue-unicons/src/icons";
 
 // add them into unicon
-Unicon.add([uniMapMarker, uniArrowLeft]);
+Unicon.add([uniMapMarker, uniArrowLeft, uniCalender]);
 
 Vue.use(Unicon);
